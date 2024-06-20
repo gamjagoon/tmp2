8e37ade5ef9ce8d39d0060ecc166b7119201dc6e
Minjae Kim <51118024+gamjagoon@users.noreply.github.com>
2024-06-20 11:52:49 +0900

    Update cbor_writer.rs

--- cbor_writer.rs
+++ cbor_writer.rs
@@ -1,5 +1,19 @@
 use std::mem::size_of;
 
+fn main() {
+    let data: [u8; 32] = [0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                          0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10,
+                          0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18,
+                          0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20];
+    
+    let formatted_string = format_byte_array(&data);
+    println!("{}", formatted_string);
+}
+
+fn format_byte_array(data: &[u8]) -> String {
+    data.iter().map(|byte| format!("\\x{:02x}", byte)).collect()
+}
+
 const K_COSE_KEY_KTY_LABEL: i64 = 1;
 const K_COSE_KEY_ALG_LABEL: i64 = 3;
 const K_COSE_KEY_OPS_LABEL: i64 = 4;
